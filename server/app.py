c1492e72cf046aec91020fdd9c154568ed9e994e
Anthony Beltran <beltrananthony30@icloud.com>
2023-10-05 16:23:56 -0700

    complete

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -14,13 +14,51 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    # Query the database to get the earthquake with the given id
+    earthquake = Earthquake.query.get(id)
+
+    # Check if the earthquake with the given id exists
+    if earthquake:
+        # Format the response as a JSON string
+        response_data = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        return jsonify(response_data)
+    else:
+        # Return an error message if no earthquake is found
+        error_message = {'message': f'Earthquake {id} not found.'}
+        return make_response(jsonify(error_message), 404)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    # Query the database to get earthquakes with a magnitude greater than or equal to the parameter value
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    # Format the response as a JSON string
+    response_data = {
+        'count': len(earthquakes),
+        'quakes': [
+            {
+                'id': quake.id,
+                'location': quake.location,
+                'magnitude': quake.magnitude,
+                'year': quake.year
+            }
+            for quake in earthquakes
+        ]
+    }
+
+    return jsonify(response_data)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,9 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import Column, Integer, Float, String
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData()
+# metadata = MetaData()
 
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 
 # Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = "earthquakes"
+
+    id = Column(Integer, primary_key=True)
+    magnitude = Column(Float)
+    location = Column(String)
+    year = Column(Integer)
+
+    def __repr__(self):
+        return f"<Earthquake(id={self.id}, magnitude={self.magnitude}, location='{self.location}', year={self.year})>"
